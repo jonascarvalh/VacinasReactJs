96e3e873429319f818153d7da6aa7e6f2354d42a
jonascarvalh <olaffic@gmail.com>
2022-07-06 20:53:35 -0300

    making the menu responsive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,24 +4,28 @@ import navbarIcon from "../../assets/navbar-icon.png";
 
 export const NavbarComponent = () => {
     return (
-        <Navbar className="menu" variant="dark">
+        <Navbar className="menu" variant="dark" sticky="top" expand="lg">
             <Container>
                 <Navbar.Brand 
                 href="/">
                     <img src={navbarIcon} alt="Ícone de vacina"/>
                     BDVacinas
                 </Navbar.Brand>
-                <Nav>
-                    <NavDropdown title="Pacientes" id ="nav-dropdown">
-                        <NavDropdown.Item><Link to="/registrar-pacientes">CADASTRAR</Link></NavDropdown.Item>
-                        <NavDropdown.Item>CONSULTAR</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Vacinas" id ="nav-dropdown">
-                        <NavDropdown.Item><Link to="/vacinar-pacientes" >VACINAR</Link></NavDropdown.Item>
-                        <NavDropdown.Item>CONSULTAR</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="/" id="nav-dropdown">Sobre</Nav.Link>
-                </Nav>
+                
+                <Navbar.Toggle/>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown title="Pacientes" id="nav-dropdown">
+                            <NavDropdown.Item><Link to="/registrar-pacientes">CADASTRAR</Link></NavDropdown.Item>
+                            <NavDropdown.Item>CONSULTAR</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Vacinas" id ="nav-dropdown">
+                            <NavDropdown.Item><Link to="/vacinar-pacientes" >VACINAR</Link></NavDropdown.Item>
+                            <NavDropdown.Item>CONSULTAR</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="/" id="nav-dropdown">Sobre</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
