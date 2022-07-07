f452cb07fc23a644592d0820221114a1ca2bdbb6
jonascarvalh <olaffic@gmail.com>
2022-07-07 16:41:16 -0300

    get api data in ConsultPatient and scroll table

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -20,8 +20,8 @@ export const NavbarComponent = () => {
                         <NavDropdown.Item><Link to="/consultar-pacientes">CONSULTAR</Link></NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Vacinas" id ="nav-dropdown">
-                        <NavDropdown.Item><Link to="/vacinar-pacientes" >VACINAR</Link></NavDropdown.Item>
-                        <NavDropdown.Item>CONSULTAR</NavDropdown.Item>
+                        <NavDropdown.Item><Link to="/vacinar-pacientes">VACINAR</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to="/consultar-vacinas">CONSULTAR</Link></NavDropdown.Item>
                     </NavDropdown>
                     <Nav.Link href="/" id="nav-dropdown">Sobre</Nav.Link>
                 </Nav>
