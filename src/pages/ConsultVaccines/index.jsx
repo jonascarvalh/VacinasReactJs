ceff0d523373ed938fca2439c01cd4c9a91e3ec8
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-06 21:21:05 -0300

    add input to ConsultVaccines page

--- src/pages/ConsultVaccines/index.jsx
+++ src/pages/ConsultVaccines/index.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import { NavbarComponent } from "../../components/Navbar";
 
+import './consultvaccines.css';
 
 export default function ConsultVaccines() {
-
+    const [cpf, setCpf] = useState('');
 
     return (
         <div className="App">
             <NavbarComponent />
             
             <div className="container">
-                <div className="content">
+                <div className="content consult-vaccines">
                     <h1 className="text">Vacinas Tomadas</h1>
 
-
+                    <form>
+                        <label className="text">CPF:</label>
+                        <input type="text" placeholder="" value={cpf} onChange={ (e) => setCpf(e.target.value)} />
+                    </form>
                 </div>
             </div>
 
